a9d1d9873248b070909c8399dc77ef537fd834c1
Jordan Ohlwiler <ohlwilerj@gmail.com>
2019-08-27 16:14:43 -0600

    app2 done

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      baseballTeams: ["Angels", "Dodgers", "Astros", "Red Sox"]
+    }
+  }
   render() {
+  const baseballTeams = this.state.baseballTeams.map((element,index) => 
+    { return <h2 key={index}> {element}</h2>})
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+          <div className="App">{baseballTeams}</div>
+      );
   }
 }
 

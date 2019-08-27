8d4a7e609c8af0b6f671d4dfce57cf55d5bb0c3d
Jordan Ohlwiler <ohlwilerj@gmail.com>
2019-08-27 17:01:53 -0600

    app3 done

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      filterString: "",
+      baseballTeams: ["Angels", "Dodgers", "Astros", "Red Sox", "Yankees", "Cubs"]
+    }
+  }
+
+  handleChange(filter) {
+    this.setState ({filterString: filter});
+  }
   render() {
+  const baseballTeams = this.state.baseballTeams.filter((element,index) => 
+    { return element.includes(this.state.filterString);
+    })
+      .map((element,index) => {
+      return <h2 key={index}> {element}</h2>})
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+          <div className="App">
+          <input onChange={e => this.handleChange(e.target.value)} type="text"/>{baseballTeams}
+          </div>
     );
   }
 }
-
 export default App;
